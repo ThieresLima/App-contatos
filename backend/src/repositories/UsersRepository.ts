1c847f0067b62e807551bb7caffdab786d5cec16
Thieres <thieresllima@gmail.com>
2021-02-26 11:19:17 -0300

    users repository

---
+++ backend/src/repositories/UsersRepository.ts
@@ -0,0 +1,21 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import User from '../models/User';
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.findOne(id);
+
+    return user;
+  };
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({ where: { email }});
+
+    return user;
+  }
+}
+
+export default UsersRepository;
+
