e13d1df988bebec71c2e5b57ce5b0db9565114d4
Thieres <thieresllima@gmail.com>
2021-03-02 16:04:41 -0300

    eslint & prettier

--- contactapp/src/App.tsx
+++ contactapp/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import { StatusBar } from 'react-native';
 
-const App: React.FC = () => <View />;
+import LinearGradient from 'react-native-linear-gradient';
+
+const App: React.FC = () => {
+  return (
+    <LinearGradient style={{ flex: 1 }} colors={['#FF8292', '#6B70C2']}>
+      <StatusBar barStyle="light-content" backgroundColor="#FF8292" />
+    </LinearGradient>
+  );
+};
 
 export default App;
